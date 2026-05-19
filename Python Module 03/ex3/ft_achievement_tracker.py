83cb5f8b3fbb7fa66e3a771ebaee83a2b0837f75
lamrung07 <lamrung012345@gmail.com>
2026-05-19 16:57:55 +0200

    19/5

--- Python Module 03/ex3/ft_achievement_tracker.py
+++ Python Module 03/ex3/ft_achievement_tracker.py
@@ -1,37 +1,43 @@
 #!bin/usr/env python3
 import random
 
+
 def gen_player_achievements() -> set:
-    Acs = ['Crafting Genius', 'Strategist', 'World Savior', 'Speed Runner', 'Survivor',
-'Master Explorer', 'Treasure Hunter', 'Unstoppable', 'First Steps', 'Collector Supreme', 'Untouchable',
-'Sharp Mind', 'Boss Slayer']
+    Acs = ['Crafting Genius', 'Strategist', 'World Savior', 'Speed Runner',
+            'Survivor', 'Master Explorer', 'Treasure Hunter', 'Unstoppable',
+            'First Steps', 'Collector Supreme', 'Untouchable', 'Sharp Mind',
+            'Boss Slayer']
     acs_number = random.randint(0, len(Acs))
     Player_acs = random.sample(Acs, acs_number)
     return set(Player_acs)
-    
+
+
 def personal_achievement(person, others) -> set:
     for other in others:
         if other != person:
             person = person.difference(other)
     return person
 
+
 def main() -> None:
-    Achievements = {'Crafting Genius', 'Strategist', 'World Savior', 'Speed Runner', 'Survivor',
-'Master Explorer', 'Treasure Hunter', 'Unstoppable', 'First Steps', 'Collector Supreme', 'Untouchable',
-'Sharp Mind', 'Boss Slayer'}
-    All_acs = Achievements
+    All_acs = {'Crafting Genius', 'Strategist', 'World Savior', 
+                'Speed Runner', 'Survivor', 'Master Explorer', 
+                'Treasure Hunter', 'Unstoppable', 'First Steps',
+                'Collector Supreme', 'Untouchable', 'Sharp Mind', 
+                'Boss Slayer'}
+    Dis_acs = All_acs
     Players = ['Alice', 'Bob', 'Charlie', 'Dylan']
-    Acs     = [None] * len(Players)
+    Acs = [None] * len(Players)
     i = 0
-    Com_acs = All_acs
+    Com_acs = Dis_acs
     for i in range(0, len(Acs)):
         Acs[i] = gen_player_achievements()
         if i != 0:
-            All_acs = Acs[i].union(Acs[i - 1])
+            Dis_acs = Acs[i].union(Acs[i - 1])
         Com_acs = Acs[i].intersection(Com_acs)
         print(f"Player {Players[i]}: {Acs[i]}")
         i += 1
-    print(f"\nAll distinct achievements: {All_acs}\n")
+    print(f"\nAll distinct achievements: {Dis_acs}\n")
     i = 0
     print(f"\nCommon achievements: {Com_acs}\n")
     for i in range(0, len(Players)):
@@ -39,8 +45,9 @@ def main() -> None:
     print("\n")
     i = 0
     for i in range(0, len(Players)):
-        print(f"{Players[i]} is missing: {Achievements.difference(Acs[i])}")
-        
+        print(f"{Players[i]} is missing: {All_acs.difference(Acs[i])}")
+
+
 if __name__ == "__main__":
     print("=== Achievement Tracker System ===\n")
     main()
